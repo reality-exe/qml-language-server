0628e89473ce7cee6cc09731d488362a71b5e4b9
reality-exe <reality@uninetwork.net>
2026-04-16 01:51:00 -0400

    create nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Go Template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        nativeBuildInputs = with pkgs; [
+          go
+          gopls
+        ];
+        buildInputs = with pkgs; [ ];
+      in
+      {
+        devShells.default = pkgs.mkShell { inherit nativeBuildInputs buildInputs; };
+
+        packages.default = pkgs.buildGoModule rec {
+          name = "qml-language-server";
+          src = ./.;
+
+          inherit buildInputs;
+
+          vendorHash = "sha256-J+0kFTKgluf+mabJepW+MGXUdHqYLFaUVAZEWcyHmyk=";
+        };
+      }
+    );
+}
